b6460d80b3041ee7891cefa86dd84c7a410f461b
Jason rader <Jasonmrader@outlook.com>
2023-08-26 12:48:04 -0400

    UI changes to settings form

--- FantasyFootballAuctionDraftAssistant/frmSettings.Designer.cs
+++ FantasyFootballAuctionDraftAssistant/frmSettings.Designer.cs
@@ -29,177 +29,100 @@
         private void InitializeComponent()
         {
             btnLoadExcelData = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            panel1 = new Panel();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            panel4 = new Panel();
-            panel5 = new Panel();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
-            checkBox3 = new CheckBox();
-            checkBox4 = new CheckBox();
-            checkBox5 = new CheckBox();
+            cbName = new CheckBox();
+            cbPosition = new CheckBox();
+            cbTeam = new CheckBox();
+            cbBye = new CheckBox();
+            cbValue = new CheckBox();
             listBox1 = new ListBox();
             panel6 = new Panel();
             panel7 = new Panel();
-            textBox6 = new TextBox();
-            textBox7 = new TextBox();
-            button1 = new Button();
-            label1 = new Label();
             label2 = new Label();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            panel3.SuspendLayout();
-            panel4.SuspendLayout();
-            panel5.SuspendLayout();
+            label1 = new Label();
+            button1 = new Button();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            cbYear = new CheckBox();
+            nudName = new NumericUpDown();
+            nudPosition = new NumericUpDown();
+            nudTeam = new NumericUpDown();
+            nudBye = new NumericUpDown();
+            nudValue = new NumericUpDown();
+            nudYear = new NumericUpDown();
             panel6.SuspendLayout();
             panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudName).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudPosition).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudTeam).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudBye).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudValue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudYear).BeginInit();
             SuspendLayout();
             // 
             // btnLoadExcelData
             // 
-            btnLoadExcelData.Location = new Point(28, 256);
+            btnLoadExcelData.Location = new Point(13, 268);
             btnLoadExcelData.Name = "btnLoadExcelData";
-            btnLoadExcelData.Size = new Size(363, 40);
+            btnLoadExcelData.Size = new Size(193, 40);
             btnLoadExcelData.TabIndex = 0;
             btnLoadExcelData.Text = "Load Excel Data";
             btnLoadExcelData.UseVisualStyleBackColor = true;
             btnLoadExcelData.Click += btnLoadExcelData_Click;
             // 
-            // textBox1
+            // cbName
             // 
-            textBox1.Location = new Point(0, 28);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            cbName.Appearance = Appearance.Button;
+            cbName.Location = new Point(13, 21);
+            cbName.Name = "cbName";
+            cbName.Size = new Size(75, 23);
+            cbName.TabIndex = 3;
+            cbName.Text = "Name";
+            cbName.TextAlign = ContentAlignment.MiddleCenter;
+            cbName.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // cbPosition
             // 
-            textBox2.Location = new Point(0, 25);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 2;
+            cbPosition.Appearance = Appearance.Button;
+            cbPosition.Location = new Point(13, 59);
+            cbPosition.Name = "cbPosition";
+            cbPosition.Size = new Size(75, 23);
+            cbPosition.TabIndex = 3;
+            cbPosition.Text = "Position";
+            cbPosition.TextAlign = ContentAlignment.MiddleCenter;
+            cbPosition.UseVisualStyleBackColor = true;
             // 
-            // textBox3
+            // cbTeam
             // 
-            textBox3.Location = new Point(0, 25);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
-            textBox3.TabIndex = 2;
+            cbTeam.Appearance = Appearance.Button;
+            cbTeam.Location = new Point(13, 97);
+            cbTeam.Name = "cbTeam";
+            cbTeam.Size = new Size(75, 23);
+            cbTeam.TabIndex = 3;
+            cbTeam.Text = "Team";
+            cbTeam.TextAlign = ContentAlignment.MiddleCenter;
+            cbTeam.UseVisualStyleBackColor = true;
             // 
-            // textBox4
+            // cbBye
             // 
-            textBox4.Location = new Point(0, 25);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
-            textBox4.TabIndex = 2;
+            cbBye.Appearance = Appearance.Button;
+            cbBye.Location = new Point(13, 135);
+            cbBye.Name = "cbBye";
+            cbBye.Size = new Size(75, 23);
+            cbBye.TabIndex = 3;
+            cbBye.Text = "Bye";
+            cbBye.TextAlign = ContentAlignment.MiddleCenter;
+            cbBye.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // cbValue
             // 
-            textBox5.Location = new Point(0, 25);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(100, 23);
-            textBox5.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(checkBox1);
-            panel1.Controls.Add(textBox1);
-            panel1.Location = new Point(25, 40);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(101, 66);
-            panel1.TabIndex = 3;
-            // 
-            // panel2
-            // 
-            panel2.Controls.Add(textBox2);
-            panel2.Controls.Add(checkBox2);
-            panel2.Location = new Point(156, 40);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(101, 66);
-            panel2.TabIndex = 3;
-            // 
-            // panel3
-            // 
-            panel3.Controls.Add(textBox3);
-            panel3.Controls.Add(checkBox3);
-            panel3.Location = new Point(287, 40);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(101, 66);
-            panel3.TabIndex = 3;
-            // 
-            // panel4
-            // 
-            panel4.Controls.Add(textBox4);
-            panel4.Controls.Add(checkBox4);
-            panel4.Location = new Point(86, 122);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(101, 66);
-            panel4.TabIndex = 3;
-            // 
-            // panel5
-            // 
-            panel5.Controls.Add(checkBox5);
-            panel5.Controls.Add(textBox5);
-            panel5.Location = new Point(217, 122);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(101, 66);
-            panel5.TabIndex = 3;
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(3, 3);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(58, 19);
-            checkBox1.TabIndex = 3;
-            checkBox1.Text = "Name";
-            checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(3, 3);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(69, 19);
-            checkBox2.TabIndex = 3;
-            checkBox2.Text = "Position";
-            checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(3, 3);
-            checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(54, 19);
-            checkBox3.TabIndex = 3;
-            checkBox3.Text = "Team";
-            checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            checkBox4.AutoSize = true;
-            checkBox4.Location = new Point(0, 3);
-            checkBox4.Name = "checkBox4";
-            checkBox4.Size = new Size(45, 19);
-            checkBox4.TabIndex = 3;
-            checkBox4.Text = "Bye";
-            checkBox4.UseVisualStyleBackColor = true;
-            // 
-            // checkBox5
-            // 
-            checkBox5.AutoSize = true;
-            checkBox5.Location = new Point(3, 3);
-            checkBox5.Name = "checkBox5";
-            checkBox5.Size = new Size(54, 19);
-            checkBox5.TabIndex = 3;
-            checkBox5.Text = "Value";
-            checkBox5.UseVisualStyleBackColor = true;
+            cbValue.Appearance = Appearance.Button;
+            cbValue.Location = new Point(13, 173);
+            cbValue.Name = "cbValue";
+            cbValue.Size = new Size(75, 23);
+            cbValue.TabIndex = 3;
+            cbValue.Text = "Value";
+            cbValue.TextAlign = ContentAlignment.MiddleCenter;
+            cbValue.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
@@ -213,15 +136,22 @@
             // panel6
             // 
             panel6.BackColor = Color.FromArgb(224, 224, 224);
-            panel6.Controls.Add(panel1);
+            panel6.Controls.Add(nudYear);
+            panel6.Controls.Add(nudValue);
+            panel6.Controls.Add(nudBye);
+            panel6.Controls.Add(nudTeam);
+            panel6.Controls.Add(nudPosition);
+            panel6.Controls.Add(nudName);
+            panel6.Controls.Add(cbYear);
+            panel6.Controls.Add(cbValue);
+            panel6.Controls.Add(cbBye);
+            panel6.Controls.Add(cbTeam);
+            panel6.Controls.Add(cbPosition);
+            panel6.Controls.Add(cbName);
             panel6.Controls.Add(btnLoadExcelData);
-            panel6.Controls.Add(panel5);
-            panel6.Controls.Add(panel2);
-            panel6.Controls.Add(panel4);
-            panel6.Controls.Add(panel3);
             panel6.Location = new Point(12, 14);
             panel6.Name = "panel6";
-            panel6.Size = new Size(431, 351);
+            panel6.Size = new Size(224, 351);
             panel6.TabIndex = 5;
             // 
             // panel7
@@ -233,42 +163,10 @@
             panel7.Controls.Add(textBox7);
             panel7.Controls.Add(textBox6);
             panel7.Controls.Add(listBox1);
-            panel7.Location = new Point(471, 14);
+            panel7.Location = new Point(252, 14);
             panel7.Name = "panel7";
             panel7.Size = new Size(394, 351);
             panel7.TabIndex = 6;
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(25, 43);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(100, 23);
-            textBox6.TabIndex = 5;
-            // 
-            // textBox7
-            // 
-            textBox7.Location = new Point(25, 107);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(100, 23);
-            textBox7.TabIndex = 5;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(25, 178);
-            button1.Name = "button1";
-            button1.Size = new Size(100, 32);
-            button1.TabIndex = 6;
-            button1.Text = "Save Team";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(25, 25);
-            label1.Name = "label1";
-            label1.Size = new Size(70, 15);
-            label1.TabIndex = 7;
-            label1.Text = "Team Name";
             // 
             // label2
             // 
@@ -279,49 +177,121 @@
             label2.TabIndex = 7;
             label2.Text = "Owner";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Team Name";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(25, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 32);
+            button1.TabIndex = 6;
+            button1.Text = "Save Team";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(25, 107);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(25, 43);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 5;
+            // 
+            // cbYear
+            // 
+            cbYear.Appearance = Appearance.Button;
+            cbYear.CheckAlign = ContentAlignment.TopRight;
+            cbYear.Location = new Point(13, 211);
+            cbYear.Name = "cbYear";
+            cbYear.Size = new Size(75, 23);
+            cbYear.TabIndex = 4;
+            cbYear.Text = "Year";
+            cbYear.TextAlign = ContentAlignment.MiddleCenter;
+            cbYear.UseVisualStyleBackColor = true;
+            // 
+            // nudName
+            // 
+            nudName.Location = new Point(109, 21);
+            nudName.Name = "nudName";
+            nudName.Size = new Size(67, 23);
+            nudName.TabIndex = 5;
+            // 
+            // nudPosition
+            // 
+            nudPosition.Location = new Point(109, 59);
+            nudPosition.Name = "nudPosition";
+            nudPosition.Size = new Size(67, 23);
+            nudPosition.TabIndex = 5;
+            // 
+            // nudTeam
+            // 
+            nudTeam.Location = new Point(109, 97);
+            nudTeam.Name = "nudTeam";
+            nudTeam.Size = new Size(67, 23);
+            nudTeam.TabIndex = 5;
+            // 
+            // nudBye
+            // 
+            nudBye.Location = new Point(109, 135);
+            nudBye.Name = "nudBye";
+            nudBye.Size = new Size(67, 23);
+            nudBye.TabIndex = 5;
+            // 
+            // nudValue
+            // 
+            nudValue.Location = new Point(109, 173);
+            nudValue.Name = "nudValue";
+            nudValue.Size = new Size(67, 23);
+            nudValue.TabIndex = 5;
+            // 
+            // nudYear
+            // 
+            nudYear.Location = new Point(109, 211);
+            nudYear.Name = "nudYear";
+            nudYear.Size = new Size(67, 23);
+            nudYear.TabIndex = 5;
+            // 
             // frmSettings
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(898, 385);
+            ClientSize = new Size(669, 385);
             Controls.Add(panel7);
             Controls.Add(panel6);
             Name = "frmSettings";
             Text = "frmSettings";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            panel4.ResumeLayout(false);
-            panel4.PerformLayout();
-            panel5.ResumeLayout(false);
-            panel5.PerformLayout();
             panel6.ResumeLayout(false);
             panel7.ResumeLayout(false);
             panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudName).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudPosition).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudTeam).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudBye).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudValue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudYear).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnLoadExcelData;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private Panel panel1;
-        private CheckBox checkBox1;
-        private Panel panel2;
-        private CheckBox checkBox2;
-        private Panel panel3;
-        private CheckBox checkBox3;
-        private Panel panel4;
-        private CheckBox checkBox4;
-        private Panel panel5;
-        private CheckBox checkBox5;
+        private CheckBox cbName;
+        private CheckBox cbPosition;
+        private CheckBox cbTeam;
+        private CheckBox cbBye;
+        private CheckBox cbValue;
         private ListBox listBox1;
         private Panel panel6;
         private Panel panel7;
@@ -330,5 +300,12 @@
         private Button button1;
         private TextBox textBox7;
         private TextBox textBox6;
+        private NumericUpDown nudYear;
+        private NumericUpDown nudValue;
+        private NumericUpDown nudBye;
+        private NumericUpDown nudTeam;
+        private NumericUpDown nudPosition;
+        private NumericUpDown nudName;
+        private CheckBox cbYear;
     }
 }
